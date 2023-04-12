19a92f77148adcc393a114d0186decdf5a3b78b7
Jean-Marc <jmarc.dinghin@Jean-Marcs-MacBook-Pro.local>
2023-04-12 11:18:55 -0400

    add API functions

---
+++ app/components/Results.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { battle } from "../utils/api";
+
+export default class Results extends React.Component {
+  componentDidMount() {
+    const { playerOne, playerTwo } = this.props;
+
+    battle([playerOne, playerTwo]).then((players) => {
+      console.log("data", players);
+    });
+  }
+  render() {
+    return (
+      <div>
+        Results
+        <pre>{JSON.stringify(this.props, null, 1)}</pre>
+      </div>
+    );
+  }
+}
